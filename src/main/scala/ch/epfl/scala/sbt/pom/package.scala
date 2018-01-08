1e18a5bf71087a96622ec336efb066c0c4b2f55c
jvican <jorgevc@fastmail.es>
2018-01-09 00:46:59 +0100

    Reuse default locator and session

--- src/main/scala/ch/epfl/scala/sbt/pom/package.scala
+++ src/main/scala/ch/epfl/scala/sbt/pom/package.scala
@@ -3,39 +3,18 @@ package ch.epfl.scala.sbt
 import java.io.File
 
 import org.apache.maven.repository.internal._
-import org.eclipse.aether.impl.{
-  ArtifactDescriptorReader,
-  DefaultServiceLocator,
-  MetadataGeneratorFactory,
-  VersionRangeResolver,
-  VersionResolver
-}
 import org.eclipse.aether.repository.LocalRepository
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory
 import org.eclipse.aether.spi.connector.transport.TransporterFactory
 import org.eclipse.aether.transport.file.FileTransporterFactory
 import org.eclipse.aether.transport.http.HttpTransporterFactory
-import org.eclipse.aether.{
-  DefaultRepositorySystemSession,
-  RepositorySystem,
-  RepositorySystemSession
-}
+import org.eclipse.aether.{RepositorySystem, RepositorySystemSession}
 
 /** Helper methods for dealing with starting up Aether. */
 package object pom {
   def newRepositorySystemImpl: RepositorySystem = {
-    val locator = new DefaultServiceLocator()
-    locator.addService(classOf[ArtifactDescriptorReader],
-                       classOf[DefaultArtifactDescriptorReader])
-    locator.addService(classOf[VersionResolver],
-                       classOf[DefaultVersionResolver])
-    locator.addService(classOf[VersionRangeResolver],
-                       classOf[DefaultVersionRangeResolver])
-    locator.addService(classOf[MetadataGeneratorFactory],
-                       classOf[SnapshotMetadataGeneratorFactory])
-    locator.addService(classOf[MetadataGeneratorFactory],
-                       classOf[VersionsMetadataGeneratorFactory])
+    val locator = MavenRepositorySystemUtils.newServiceLocator()
     locator.addService(classOf[RepositoryConnectorFactory],
                        classOf[BasicRepositoryConnectorFactory])
     locator.addService(classOf[TransporterFactory],
@@ -47,7 +26,7 @@ package object pom {
 
   def newSessionImpl(system: RepositorySystem,
                      localRepoDir: File): RepositorySystemSession = {
-    val session = new DefaultRepositorySystemSession()
+    val session = MavenRepositorySystemUtils.newSession()
     val localRepo: LocalRepository = new LocalRepository(
       localRepoDir.getAbsolutePath)
     session.setLocalRepositoryManager(
